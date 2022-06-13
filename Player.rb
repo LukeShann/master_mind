53e75c7d08f001a067913e2282c63d772d484cc8
Luke <luke.goddard44@gmail.com>
2022-06-13 15:08:46 +0100

    add player code selection

--- Player.rb
+++ Player.rb
@@ -1,12 +1,26 @@
 require_relative "screen"
 
 class Player
+  include Screen
 end
 
 class HumanPlayer < Player
   def make_code
-    puts 'human making code'
+    def is_valid?(code)
+      return false if code.length != 4
+      return false if code.uniq.length != 4
+      code.all? { |digit| (1..6).include?(digit) }
+    end
+
+    print_message("Enter a 4 digit code")
+    code = []
+    until is_valid?(code) do
+      code = gets.chomp.split('')
+      code.map!(&:to_i)
+    end
+    code
   end
+
 end
 
 class ComputerPlayer < Player
@@ -21,5 +35,7 @@ class ComputerPlayer < Player
         end
       end
     end
+    code
+    print_message("Computer has selected a code")
   end
 end
